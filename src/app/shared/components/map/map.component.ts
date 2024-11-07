b96b86836f754bdca8e853a1f4d500d6e20aefc5
leoaltemari <leo.altemari@gmail.com>
2024-11-07 00:48:42 -0300

    Fix prettier formatting html wrongly

--- src/app/shared/components/map/map.component.ts
+++ src/app/shared/components/map/map.component.ts
@@ -1,17 +1,22 @@
-import { CommonModule } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { Component, Input, OnInit } from "@angular/core";
 
-import { CellComponent } from '@components/cell/cell.component';
-import { Cell, Map, MapRow } from '@models';
+import { CellComponent } from "@components/cell/cell.component";
+import { Cell, Map, MapRow } from "@models";
 
 @Component({
-  selector: 'app-map',
+  selector: "app-map",
   standalone: true,
   imports: [CommonModule, CellComponent],
-  templateUrl: './map.component.html',
-  styleUrl: './map.component.scss',
+  templateUrl: "./map.component.html",
+  styleUrl: "./map.component.scss",
 })
 export class MapComponent implements OnInit {
+  /**
+   * Dimension sets the number of cells the map will have on the rows and columns.
+   * A dimension of 5 means that the map will have 5 rows with 5 cells each (total of 25 cells),
+   * as a matrix 5x5.
+   * */
   @Input() dimension = 5;
 
   mapCells: Map = {
@@ -23,12 +28,14 @@ export class MapComponent implements OnInit {
   }
 
   private buildMap(): void {
+    /** Creates all map rows */
     for (let i = 0; i < this.dimension; i++) {
       const row: MapRow = {
         id: i.toString(),
         cells: [],
       };
 
+      /** Creates each cell of the row 'i' */
       for (let j = 0; j < this.dimension; j++) {
         row.cells.push({
           id: i.toString() + j.toString(),
