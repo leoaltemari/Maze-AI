09b75c183d218845e57bcd4b91fd9a1db7b4742d
leoaltemari <leo.altemari@gmail.com>
2024-11-07 17:04:49 -0300

    Adds background to host at app-componnet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
-import { CellType } from './shared/models/cell.model';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CellComponent } from './shared/components/cell/cell.component';
+
+import { CellComponent } from '@components/cell/cell.component';
 import { MapComponent } from '@components/map/map.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, CellComponent, MapComponent],
+  host: {
+    class: 'bg-grey-3',
+  },
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  CellType = CellType;
-}
+export class AppComponent {}
