556a6b7007583ee0f00868201a93f2c981ef43ab
leoaltemari <leo.altemari@gmail.com>
2024-11-07 17:05:32 -0300

    Enhances cell-component to accept position with the new Position type

--- src/app/shared/components/cell/cell.component.ts
+++ src/app/shared/components/cell/cell.component.ts
@@ -1,20 +1,36 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
-import { Cell, CellType } from '@models';
+import { Cell, CellActions, CellType, Position } from '@models';
+import { MapInteractionService } from '@services';
 
 @Component({
   selector: 'app-cell',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './cell.component.html',
-  styleUrl: './cell.component.scss'
+  styleUrl: './cell.component.scss',
 })
 export class CellComponent implements Cell {
   @Input({ required: true }) type: CellType = CellType.Empty;
-  @Input() x: number | undefined;
-  @Input() y: number | undefined;
+  @Input() position: Position = { x: 0, y: 0 };
 
   public readonly cellTypeEnum = CellType;
 
+  constructor(private readonly mapInteractionService: MapInteractionService) {}
+
+  private getNewCellType(): CellType {
+    switch (this.mapInteractionService.currentCellAction) {
+      case CellActions.Source:
+        break;
+
+      default:
+        break;
+    }
+    return CellType.Empty;
+  }
+
+  onCellClick(): void {
+    this.type = this.getNewCellType();
+  }
 }
--- src/app/shared/components/map/map.component.ts
+++ src/app/shared/components/map/map.component.ts
@@ -1,15 +1,16 @@
-import { CommonModule } from "@angular/common";
-import { Component, Input, OnInit } from "@angular/core";
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
 
-import { CellComponent } from "@components/cell/cell.component";
-import { Cell, Map, MapRow } from "@models";
+import { CellComponent } from '@components/cell/cell.component';
+import { MapInteractionService, MapBuilderService } from '@services';
+import { IMap } from '@models';
 
 @Component({
-  selector: "app-map",
+  selector: 'app-map',
   standalone: true,
   imports: [CommonModule, CellComponent],
-  templateUrl: "./map.component.html",
-  styleUrl: "./map.component.scss",
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.scss',
 })
 export class MapComponent implements OnInit {
   /**
@@ -19,31 +20,17 @@ export class MapComponent implements OnInit {
    * */
   @Input() dimension = 5;
 
-  mapCells: Map = {
-    rows: [],
-  };
+  mapCells: IMap | undefined;
+
+  constructor(
+    private readonly mapInteractionService: MapInteractionService,
+    private readonly mapBuilderService: MapBuilderService,
+  ) {}
 
   ngOnInit(): void {
-    this.buildMap();
-  }
+    this.mapBuilderService.buildInitialMapMatrix(this.dimension);
+    this.mapBuilderService.buildMapObjectFromMatrix();
 
-  private buildMap(): void {
-    /** Creates all map rows */
-    for (let i = 0; i < this.dimension; i++) {
-      const row: MapRow = {
-        id: i.toString(),
-        cells: [],
-      };
-
-      /** Creates each cell of the row 'i' */
-      for (let j = 0; j < this.dimension; j++) {
-        row.cells.push({
-          id: i.toString() + j.toString(),
-          cell: new Cell(i, j),
-        });
-      }
-
-      this.mapCells.rows?.push(row);
-    }
+    this.mapCells = this.mapBuilderService.mapCells;
   }
 }
