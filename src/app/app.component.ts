235942afe1f619f18e0f9cbfaf7619e55c74c66b
leoaltemari <leo.altemari@gmail.com>
2024-11-08 13:32:39 -0300

    Updates app-component to have only the .ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { CellComponent } from '@components/cell/cell.component';
-import { MapComponent } from '@components/map/map.component';
+
+import { MainComponent } from './routes/main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CellComponent, MapComponent],
-  host: {
-    class: 'bg-grey-3',
-  },
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, CellComponent, MainComponent],
+  template: '<app-main></app-main>',
 })
 export class AppComponent {}
