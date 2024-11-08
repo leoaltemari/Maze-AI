b42549456348035b0e12214d0c0407722d45a218
leoaltemari <leo.altemari@gmail.com>
2024-11-07 23:06:04 -0300

    Enhances function that compare positions to accept undefined

--- src/app/shared/utils/compare.util.ts
+++ src/app/shared/utils/compare.util.ts
@@ -1,5 +1,13 @@
 import { Position } from '@models';
 
-export const equalPositions = (pos1: Position, pos2: Position) => {
+
+export const equalPositions = (
+  pos1: Position | undefined,
+  pos2: Position | undefined,
+) => {
+  if (!pos1 || !pos2) {
+    return false;
+  }
+
   return pos1.x === pos2.x && pos1.y === pos2.y;
 };
