d4be32321b5326aa0692fd54659ec80bb028aec9
leoaltemari <leo.altemari@gmail.com>
2024-11-12 13:09:05 -0300

    Creates new buttons to clear any type of cell

--- src/app/routes/main/side-bar/side-bar.component.ts
+++ src/app/routes/main/side-bar/side-bar.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 
-import { Algorithms, TurnCellInto } from '@models';
+import { Algorithms, CellType, TurnCellInto } from '@models';
 import { MapBuilderService, MapInteractionService, SearchService } from '@services';
 
 @Component({
@@ -27,6 +27,18 @@ export class SideBarComponent {
   protected algorithmListExpanded = true;
   protected selectedAlgorithm = this.mapInteractionService.selectedAlgorithmAsSignal;
 
+  protected clearWalls = () => this.mapBuilderService.clear(CellType.Wall);
+  protected clearPath = () => {
+    this.mapBuilderService.clear(CellType.Path);
+    this.mapBuilderService.clear(CellType.Expanded);
+  };
+  protected clearAll = () => {
+    this.clearWalls();
+    this.clearPath();
+    this.mapBuilderService.clear(CellType.Source);
+    this.mapBuilderService.clear(CellType.Target);
+  };
+
   turnCellInto(value: TurnCellInto): void {
     this.mapInteractionService.turnCellInto = value;
   }
@@ -41,10 +53,6 @@ export class SideBarComponent {
     };
 
     this.mapInteractionService.selectedAlgorithm = optionToAlgoMap[option];
-  }
-
-  clearWallsOnClick(): void {
-    this.mapBuilderService.clearAllMapWalls();
   }
 
   runSearchAlgorithm() {
